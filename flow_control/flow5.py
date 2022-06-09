49fc9abdabf8d9239673369e71b1432d643100a8
siwarkar <s.iwakar@gmail.com>
2022-06-09 10:52:24 +0530

    update flow 5 file

---
+++ flow_control/flow5.py
@@ -0,0 +1,17 @@
+for i in range(5):
+    if i == 2:
+        break
+    else:
+        print(i)
+print("------------------------------------------------")
+for i in range(5):
+    if i == 2:
+        continue
+    else:
+        print(i)
+print("------------------------------------")
+for i in range(5):
+    if i == 2:
+        pass
+    else:
+        print(i)
