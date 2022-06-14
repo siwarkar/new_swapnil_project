004e3f52f8b7dea0b3ff4381b59f9dcfdd478a39
siwarkar <s.iwakar@gmail.com>
2022-06-14 14:22:57 +0530

    i create module directorie and one pyfile

---
+++ module/module.py
@@ -0,0 +1,35 @@
+"""
+ built module
+ when we want to use a module we need to import that module
+import math
+print(math.factorial(4))
+print(math.pi)
+print("----------------------------------------------")
+import math as m
+print(m.factorial(5))
+print(m.pi)
+if you want to access all the member,variable ,function
+import math
+from math import *
+print(math.factorial(4))
+print(math.pi)
+now i want  to access specific method in module
+import math
+from math import factorial,sqrt,pi
+print(math.factorial(5))
+print(math.sqrt(25))
+print(math.pi)
+if you want  member alise
+
+import math
+print(dir())
+print('----------------------')
+from math import *
+print(dir())
+
+"""
+import math
+
+
+
+
